e165c2f7c70670a690688064a34f7b4de8ba2be5
Jonathan Papineau <jonapap2001@gmail.com>
2018-12-04 11:07:34 -0500

    Added sendln function to print arbitary number of arguments to Serial

--- Color_correct_omniwheel/Functions.h
+++ Color_correct_omniwheel/Functions.h
@@ -2,23 +2,36 @@
 #define FUNCTION_H
 
 #include <Arduino.h>
+#include "DEBUG.h"
 
-inline double toRadians(double x){
-	return x*(PI/180);
+inline double toRadians(double x) {
+	return x * (PI / 180);
 }
 
-inline double toDegrees(double x){
-	return x/(PI/180);
+inline double toDegrees(double x) {
+	return x / (PI / 180);
 }
 
-template <typename T>
-inline T mod(T x, int y){
-	return x-y*floor(x/y);
+template<typename T>
+inline T mod(T x, int y) {
+	return x - y * floor(x / y);
 }
 
-template <typename T, typename U>
-inline boolean inRange(T x, T y, U r){
+template<typename T, typename U>
+inline boolean inRange(T x, T y, U r) {
 	return abs(x-y) <= r;
+}
+
+template<typename T>
+void sendln(T v) {
+	DEBUG_PRINT(v);
+	DEBUG_PRINTLN("");
+}
+
+template<typename T, typename ... Args>
+void sendln(T first, Args ... args) {
+	DEBUG_PRINT(first);
+	sendln(args...);
 }
 
 #endif
