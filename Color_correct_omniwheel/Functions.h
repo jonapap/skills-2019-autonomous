8fde9bf6f3033d5f2e434edb5d1d93205e9ba5f8
Jonathan Papineau <jonapap2001@gmail.com>
2019-04-05 11:38:08 -0400

    Added function to get freeMemory

--- Color_correct_omniwheel/Functions.h
+++ Color_correct_omniwheel/Functions.h
@@ -39,4 +39,22 @@ inline boolean isZero(int a){
 	return a == 0 ? true : false;
 }
 
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char* sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif  // __arm__
+
+inline int freeMemory() {
+  char top;
+#ifdef __arm__
+  return &top - reinterpret_cast<char*>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+  return &top - __brkval;
+#else  // __arm__
+  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif  // __arm__
+}
+
 #endif
