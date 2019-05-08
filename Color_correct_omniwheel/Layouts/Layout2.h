45cf96d4c9a922aeebccafd0d63328a814f98e29
Jonathan Papineau <jonapap2001@gmail.com>
2019-05-07 22:15:56 -0400

    Adjusted layout informations for the competition

--- Color_correct_omniwheel/Layouts/Layout2.h
+++ Color_correct_omniwheel/Layouts/Layout2.h
@@ -1,15 +1,15 @@
-OmniRobot robot(14.2, 14.5, 45);
+OmniRobot robot(14.5, 14.5, 45);
 
-Square redSquare( { 18.5, 75.8 }, 135, Square::red);
-Square blueSquare( { 75.8, 75.8 }, 45, Square::blue);
-Square yellowSquare( { 75.8, 18.5 }, 315, Square::yellow);
+Square redSquare( { 18.5, 76 }, 135, Square::red);
+Square blueSquare( { 76, 76 }, 45, Square::blue);
+Square yellowSquare( { 76.5, 19 }, 315, Square::yellow);
 
-Block blocks[] = { Block( { 41.75, 35.5 }, 0, RIGHTAPPROACH, redSquare), //block 1
-Block( { 41.75, 47.5}, 0, LEFTAPPROACH, blueSquare), //block 2
-Block( { 52.5, 59.5 }, 180, RIGHTAPPROACH, blueSquare),  //block 3
-Block( { 52.5, 47.5}, 180, RIGHTAPPROACH, redSquare), //block 4
-Block( { 41.75, 59.5}, 0, LEFTAPPROACH, yellowSquare), //block 5
-Block( { 52.5, 35.5 }, 180, LEFTAPPROACH, yellowSquare) //block 6
+Block blocks[] = { Block( { 41.75, 35 }, 0, RIGHTAPPROACH, redSquare), //block 1
+Block( { 41.75, 47}, 0, LEFTAPPROACH, blueSquare), //block 2
+Block( { 52.75, 59 }, 180, RIGHTAPPROACH, blueSquare),  //block 3
+Block( { 52.75, 47}, 180, RIGHTAPPROACH, redSquare), //block 4
+Block( { 41.75, 59}, 0, LEFTAPPROACH, yellowSquare), //block 5
+Block( { 52.75, 35 }, 180, LEFTAPPROACH, yellowSquare) //block 6
 		};
 
 void setupCourt() {
